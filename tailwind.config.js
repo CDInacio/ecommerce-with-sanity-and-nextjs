dc36f8400dd1bcc6fe38dec90640c1c407b690bc
claudioDantas <claudiodantas1996@gmail.com>
2022-07-21 11:07:41 -0300

    add files

---
+++ tailwind.config.js
@@ -0,0 +1,69 @@
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    screens: {
+      xxsm: "360px",
+      xsm: "420px",
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+    fontFamily: {
+      Hahmlet: ["Hahmlet", "serif"],
+      Pinyon: ["Pinyon Script", "cursive"],
+      Cormorant: ["Cormorant", "serif"],
+    },
+    colors: {
+      primary: "#BC6C25",
+      accent: "#EAE2B7",
+    },
+    extend: {
+      colors: {
+        transparent: colors.transparent,
+        white: colors.white,
+        slate: colors.slate,
+        grey: colors.gray,
+        zinc: colors.zinc,
+        neutral: colors.neutral,
+        stone: colors.stone,
+        red: colors.red,
+        orange: colors.orange,
+        amber: colors.amber,
+        yellow: colors.yellow,
+        lime: colors.lime,
+        green: colors.green,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        sky: colors.sky,
+        blue: colors.blue,
+        indigo: colors.indigo,
+        violet: colors.violet,
+        purple: colors.purple,
+        fuchsia: colors.fuchsia,
+        pink: colors.pink,
+        rose: colors.rose,
+      },
+      spacing: {
+        10: "10%",
+      },
+    },
+  },
+  plugins: [],
+};
